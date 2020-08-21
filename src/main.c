b078fb973c278a1912e1b3bbbfc0697f70a82925
Alex Ameen <alex.ameen.tx@gmail.com>
2020-08-21 12:34:17 -0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 
 /* ------------------------------------------------------------------------- */
 
+/* This is currently just used to check that headers conpile. */
 int
 main( int argc, char * argv[], char ** envp ) {
   return EXIT_SUCCESS;
