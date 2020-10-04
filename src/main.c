d79712179791fce52094023ceb2a94b15dd9efcb
alexameen <alex.ameen.tx@gmail.com>
2020-10-04 01:36:23 -0500

    typo in function name of main.c

--- src/main.c
+++ src/main.c
@@ -19,7 +19,7 @@ main( int argc, char * argv[], char ** envp )
 
   //stats_t         ven_base = { .attack = 198, .stamina = 190, .defense = 189 };
   //stats_combo_t * rankings = rank_ivs_array( ven_base, 10, GREAT_LEAGUE );
-  //fprint_iv_db_c( stdout, rankings, 10, GREAT_LEAGUE, 3, 0 );
+  //fprint_iv_rankings_c( stdout, rankings, 10, GREAT_LEAGUE, 3, 0 );
   //free( rankings );
 
   return EXIT_SUCCESS;
